b52c816c4c5f40051fdd37d6c5b21c421d5aa3b9
Neal Young <apache@neal.young.name>
2018-06-07 11:49:45 -0400

    minor relevant_districts.py corrections

--- relevant_districts.py
+++ relevant_districts.py
@@ -4,8 +4,9 @@
    input: sequence of pairs (census_block instance, ID of a district containing centroid)
    output: sequence of pairs (census_block instance, list of IDs of districts the census block intersects)
 '''
-from embedded_graph import EGraph
-from shapely.geometry import LineString #, shape?
+# from embedded_graph import EGraph
+from shapely.geometry import LineString  #, shape?
+
 
 class Relevant_District_Item:
     def __init__(self, ID):
@@ -14,17 +15,22 @@ class Relevant_District_Item:
 
 def gen(census_block_plus_district_ID_collection, district_graph):
     for census_block, district_ID in census_block_plus_district_ID_collection:
-        #print("relevant districts") #for debugging
-        #district_ID is ID of corresponding vertex in district_graph
+        # print("relevant districts") #for debugging
+        # district_ID is ID of corresponding vertex in district_graph
+
+        # below misses the case when block contains a district
+        # that doesn't contain the block centroid -neal
+
         relevant_districts = {district_ID}
         neighboring_darts = district_graph.vertices[district_ID]
         for d in neighboring_darts:
             segment = district_graph.segmentmapper.id2segment[d]
             if census_block.polygon.intersects(LineString(segment)):
                 neighbor = district_graph.head[district_graph.rev(d)]
-                if neighbor < district_graph.num_vertices()-1: #if correspondings to a finite cell
+                if neighbor < district_graph.num_vertices()-1:  # if correspondings to a finite cell
                     relevant_districts.add(neighbor)
         yield census_block, [Relevant_District_Item(i) for i in relevant_districts]
+
 
 '''
 def find(iterator_maker):
