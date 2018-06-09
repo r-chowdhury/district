ac14b31a5cf7cf9dc862d0c1d91be1d4fd991164
Philip Klein <klein@brown.edu>
2018-06-09 14:55:57 -0400

    Now requires as input the boundary of the state, specified by state abbreviation and shapefile name

--- district_graph.py
+++ district_graph.py
@@ -1,13 +1,16 @@
 import embedded_graph
+import state_shape
 import Voronoi_boundaries
 
-def get(assignment_filename):
+def get(state_abbreviation, state_boundary_shapefilename, assignment_filename):
     '''returns graph and 3d locations of centers'''
     assignment_file = open(assignment_filename)
     k, n = [int(x) for x in assignment_file.readline().split()]
     C_3D = [tuple(float(x) for x in assignment_file.readline().split()) for _ in range(k)]
     G = embedded_graph.EGraph()
-    bbox = Voronoi_boundaries.find_bbox(C_3D)
+    state_boundary = state_shape.read(state_abbreviation, state_boundary_shapefilename)
+    L = sum((list(p.exterior.coords) for p in state_boundary), [])
+    bbox = Voronoi_boundaries.find_bbox(C_3D+L)
     cells = Voronoi_boundaries.power_cells(C_3D, bbox)
     for cell in cells:
         G.process_cell(cell, True) #get rid of extraneous points
