d8fb7b9005197ff33e83d059bc0c2579a41d4618
Philip Klein <klein@brown.edu>
2018-06-19 13:05:22 -0400

    Added code to find vertices for new regions bounded by given regions. Also finds those vertices representing 'infinite' regions.  Note that there should be only only infinite region, that eventually this class should find the nesting relationships between the infinite regions and make them not infinite by adding interior boundaries

--- embedded_graph.py
+++ embedded_graph.py
@@ -4,6 +4,8 @@
 
 from util import swap, vec, dotproduct, norm
 from collections import defaultdict
+from math import atan2
+from shapely.geometry import LinearRing
 
 def _remove_redundant(pts):
     "Mutates a list of points to get rid of those points that are not needed to define polygon"
@@ -51,32 +53,35 @@ class EGraph:
         self.vertices = []
         self.next = {}
         self.head = {}
-        self.links = {} #linking vertex representing an outer boundary to vertices representing inner boundaries
+        self.outer2inner = {} #mapping vertex representing an outer boundary to vertices representing inner boundaries
+        self.inner2outer = {}
                 
     def rev(self, dart_id): return dart_id ^ 1
     
     def edge(self, dart_id): return dart_id >> 1
     
-    def process_region(self, region, remove_redundant = False):
+    def add_region(self, region, remove_redundant = False):
         '''Region is a polygon with an outer boundary and possibly inner boundaries.
         To each boundary corresponds a vertex, with its own incoming darts.
         Darts incoming to a vertex correspond to segments that go around the boundary clockwise for outer boundary,
         counterclockwise for inner boundaries.
         '''
         outer_vertex = self.num_vertices() # vertex corresponding to outer boundary
-        self._process_boundary(cell.exterior, remove_redundant)
+        self._process_boundary(region.exterior, remove_redundant)
         inner_vertices = []
-        for interior_boundary in cell.interiors:
+        for interior_boundary in region.interiors:
             inner_vertices.append(self.num_vertices()) # vertex corresponding to an inner boundary
             self._process_boundary(interior_boundary, remove_redundant)
-        self.links[outer_vertex] = inner_vertices #link outer vertex to inner vertices
+        self.outer2inner[outer_vertex] = inner_vertices #link outer vertex to inner vertices
+        for v in inner_vertices:
+            self.inner2outer[v] = outer_vertex
     
     def _new_vertex(self):
         self.vertices.append([]) # create new entry in vertices table--this will record new incoming darts
         
     def _process_boundary(self, linear_ring, remove_redundant):
         '''
-        darts associated with a vertex (a cell) are clockwise-going segments around the cell
+        darts associated with a vertex (a region) are clockwise-going segments around the region
         '''
         self._new_vertex()
         pts = list(linear_ring.coords)[:-1] #omit last pt, which equals first
@@ -108,6 +113,7 @@ class EGraph:
         '''After cells have been processed, those segments not appearing twice
         form boundaries of other regions.
         '''
+        self.infinite = [] #Keep track of those regions with ccw "exterior".  These represent infinite regions.
         def head(segment): return segment[1]
         def tail(segment): return segment[0]
         #Among the reverses of segments that do appear, which ones do not appear?
@@ -129,34 +135,39 @@ class EGraph:
         segment_next = {}
         for pt0,pts_plus in incident.items():
             pts_plus.sort(key=angle_finder(pt0))
-            #new_neighbors = pts_plus if incoming(pts_plus[0]) else pts_plus[1:]+pts_plus[:1]
-            #for i in range(0,len(new_neighbors),2):
-            #    assert incoming(new_neighbors[i]) and not incoming(new_neighbors[i+1]) #i is incoming, i+1 is outgoing
-            #    segment_next[pt(new_neighbors[i]),pt0] = pt0,pt(new_neighbors[i+1])
         #Create vertices for the new polygons
         #Keep track of which darts have been assigned to vertices
         used= set()
-        for segment in outer_segments:
-            if segment not in used:
+        for initial_segment in outer_segments:
+            if initial_segment not in used:
                 #Create new vertex
                 self._new_vertex()
-                #Traverse cycle containing segment
-                first_vertex = tail(segment)
-                first_dart_id = None
-                while head(segment) != first_vertex:
+                pts = [] #Keep track of points for purpose of determining if it is an infinite region
+                #Traverse cycle containing initial_segment
+                segment = initial_segment
+                prev_dart_id = None
+                while segment != initial_segment or prev_dart_id == None:
                     used.add(segment) #record having traversed this segment
+                    pts.append(tail(segment))
                     dart_id = self._new_segment_helper(tail(segment), head(segment))
-                    if first_dart_id == None:
-                        first_dart_id = dart_id
-                    else:
+                    if prev_dart_id != None:
                         self.next[prev_dart_id] = dart_id
                     incidence_list = incident[head(segment)] #incidence list for head of current segment
                     i = incidence_list.index((tail(segment), True)) #position of current segment
-                    segment, incoming = incidence_list[(i+1) % len(incidence_list)]
+                    new_segment_head, incoming = incidence_list[(i+1) % len(incidence_list)]
+                    segment = head(segment), new_segment_head
                     assert not incoming
                     prev_dart_id = dart_id
-                next[prev_dart_id] = first_id #complete the cycle
+                self.next[prev_dart_id] = self.segmentmapper.get_id(segment) #complete the cycle
+                if LinearRing(pts).is_ccw:
+                    self.infinite.append(self.num_vertices() - 1) #Keep track of regions corresponding to infinite regions
 
+    def region_data(self, vertex_id):
+        "Given the id of the vertex corresponding to the outer boundary of a region, return the boundaries"
+        outer_boundary = [self.segmentmapper.get_segment(id)[0] for id in self.vertices[vertex_id]]
+        inner_boundaries = [[self.segmentmapper.get_segment(id)[0] for id in self.vertices[inner_vertex_id]] for inner_vertex_id in self.outer2inner.get(vertex_id, [])]
+        return outer_boundary, inner_boundaries
+        
     def next(self, id): return self.next[id]
 
     def incoming(self, vertex_id): return self.vertices[vertex_id]
@@ -169,6 +180,31 @@ class EGraph:
             
     def endpoints(self, dart_id):
         return [self.head[d] for d in [dart_id, self.rev(dart_id)]]
+
+    def neighbors(self, v):
+            return [self.head[self.rev(d)] for d in self.vertices[v]] + \
+                  (self.outer2inner[v] if v in self.outer2inner else []) + \
+                  ([self.inner2outer[v]] if v in self.inner2outer else [])
+
+    def _visit(self, root, i):
+        stack = [root]
+        while stack != []:
+            v = stack.pop()
+            if v not in self.component_number:
+                self.component_number[v] = i
+                stack.extend(self.neighbors(v))
+            #except Exception:
+            #print("exception in embedded_graph")
+        
+    def connected_components(self):
+        self.component_number = {}
+        component_reps = []
+        for vertex in range(self.num_vertices()):
+            if vertex not in self.component_number:
+                self._visit(vertex, len(component_reps))
+                component_reps.append(vertex)
+        return component_reps
+
 
 #    def dual(self):
 #        G = Egraph()
