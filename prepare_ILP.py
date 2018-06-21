55b5b6e4bdc94f1c672a4824bc267595d15a61d6
Philip Klein <klein@brown.edu>
2018-06-21 11:15:55 -0400

    reorganizing district_graph to allow its use in main_plot, fixing bugs in prepare_ILP--but not all bugs are gone

--- prepare_ILP.py
+++ prepare_ILP.py
@@ -6,11 +6,18 @@ import initial_population_assignment
 import district_graph
 import block_bfs
 import join
+import Voronoi_boundaries
+import census_block_district_intersection
 import time # for debugging
 import sys
 
 def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename, assignment_filename):
-    G, cells, C_3D = district_graph.get(state_abbreviation, state_boundary_shapefilename, assignment_filename)
+    assignment_file = open(assignment_filename)
+    k, n = [int(x) for x in assignment_file.readline().split()]
+    C_3D = [tuple(float(x) for x in assignment_file.readline().split()) for _ in range(k)]
+    bbox = district_graph.find_bbox(state_abbreviation, state_boundary_shapefilename, C_3D)
+    cells = Voronoi_boundaries.power_cells(C_3D, bbox)
+    G = district_graph.get(C_3D, cells, bbox)
     #for i in range(len(cells)):
     #    outer, inners = G.region_data(i)
     #    assert len(inners) == 0
@@ -29,6 +36,7 @@ def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename,
     print("time 2 ", time.clock() - start_time)
     start_time = time.clock()
     L = list(relevant_districts.gen(L, G))
+    L = list(census_block_district_intersection.gen(L, cells))
     print("time 3 ", time.clock() - start_time)
     start_time = time.clock()
     L = list(areas_of_intersection.gen(L, cells))
@@ -36,20 +44,14 @@ def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename,
     start_time = time.clock()
     L = list(block_bfs.get(L, cells))
     print("time 5 ", time.clock() - start_time)
-    start_time = time.clock()
-    # It might be a performance problem that the next statement materializes the whole dict
     d = {block.ID:(block, rel) for block,rel in L}
-    print("time 6 ", time.clock() - start_time)
-    start_time = time.clock()
     power_diagram_result = {id:pop_assignment for id, pop_assignment in initial_population_assignment.gen(assignment_filename)}
     #Here, item.population is the population of the block assigned to the district by the balanced solution
     #If the block is not represented in power_diagram_result, the block's population is zero.
     for block, rel in L:
             for item in rel:
-                item.population = power_diagram_result[block.id].get(item.ID, 0) if id in power_diagram_result else 0
+                item.population = power_diagram_result[block.ID].get(item.ID, 0) if block.ID in power_diagram_result else 0
             yield block, rel
-    print("time 7 ", time.clock() - start_time)
-            
     ####it = join.gen(merge, initial_population_assignment.gen(assignment_filename), lambda x:x[0], iter(census_block_plus_collection), lambda x:x[0].ID)
     #for x in join.gen(merge, initial_population_assignment.gen(assignment_filename), lambda x:x[0],
     #                      census_block_dependents.gen(areas_of_intersection.gen((relevant_districts.gen(closest.gen(census_block.gen(shapefilename), C_3D), G)), cells), centers_2D), lambda x:x[0].ID):
