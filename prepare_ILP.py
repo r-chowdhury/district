088118467d0f0bcca9ccc276b5abe85136190653
Philip Klein <klein@brown.edu>
2018-07-29 08:53:53 -0400

    Removed line in which no output for a block with a single candidate district---maybe we could include that line after all.  Also, using k-d trees for proximity

--- prepare_ILP.py
+++ prepare_ILP.py
@@ -8,7 +8,7 @@ import block_bfs
 import join
 import Voronoi_boundaries
 import census_block_district_intersection
-import time # for debugging
+import time
 import sys
 
 def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename, assignment_filename):
@@ -18,10 +18,6 @@ def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename,
     bbox = district_graph.find_bbox(state_abbreviation, state_boundary_shapefilename, C_3D)
     cells = Voronoi_boundaries.power_cells(C_3D, bbox)
     G = district_graph.get(C_3D, cells, bbox)
-    #for i in range(len(cells)):
-    #    outer, inners = G.region_data(i)
-    #    assert len(inners) == 0
-    #    assert list(cells[i].exterior.coords)[:-1] == outer
     def merge(x,y):
         relevant_district_items = y[1]
         district2pop = x[1]
@@ -32,7 +28,7 @@ def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename,
     L = list(census_block_file.read(census_shapefilename))
     print("time 1 ", time.clock() - start_time)
     start_time = time.clock()
-    L = list(closest.gen(L, C_3D))
+    L = list(closest_kd.gen(L, C_3D))
     print("time 2 ", time.clock() - start_time)
     start_time = time.clock()
     L = list(relevant_districts.gen(L, G))
@@ -52,11 +48,6 @@ def test(state_abbreviation, state_boundary_shapefilename, census_shapefilename,
             for item in rel:
                 item.population = power_diagram_result[block.ID].get(item.ID, 0) if block.ID in power_diagram_result else 0
             yield block, rel
-    ####it = join.gen(merge, initial_population_assignment.gen(assignment_filename), lambda x:x[0], iter(census_block_plus_collection), lambda x:x[0].ID)
-    #for x in join.gen(merge, initial_population_assignment.gen(assignment_filename), lambda x:x[0],
-    #                      census_block_dependents.gen(areas_of_intersection.gen((relevant_districts.gen(closest.gen(census_block.gen(shapefilename), C_3D), G)), cells), centers_2D), lambda x:x[0].ID):
-#    for x in it:
-#        yield x
 
 
 args = sys.argv[1:]
@@ -70,13 +61,9 @@ counter = 0
 for census_block, relevant_district_items in test(state_abbreviation, state_boundary_shapefilename, shapefilename, assignment_filename):
     if counter % 10000 == 0: print("counter", counter)
     counter = counter + 1
-    if len(relevant_district_items)  == 1: continue
     fout.write(str(census_block.ID)+" ")
-    for item in relevant_district_items:   #district, area, dependent in zip(relevant_districts, areas, dependents):
+    for item in relevant_district_items:
         for value in [item.ID, item.population, item.area, item.dependee]:
             fout.write(str(value)+" ")
     fout.write("\n")
-    fout.flush() #for debugging
 fout.close()
-
-#/Users/klein/drive/District_data/RhodeIsland_census_block_data/tabblock2010_44_pophu /Users/klein/drive/District_data/RI_new_assignment.txt foo.txt
