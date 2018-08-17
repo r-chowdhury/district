3e56f86b2dfaceb32f34434c5d83b346ccd5b3d8
Philip Klein <klein@brown.edu>
2018-08-17 00:27:07 -0400

    changing over to using a dictionary mapping district IDs to Relevant_District_Items instead of a list of Relevant_District_Items that have district IDs in them.  (Now the name of the class is a bit of a misnomer.

--- relevant_districts.py
+++ relevant_districts.py
@@ -5,12 +5,25 @@
    output: sequence of pairs (census_block instance, list of IDs of districts the census block intersects)
 '''
 from shapely.geometry import LineString
-
+from census_block import block_types
+from collections import deque
 
 class Relevant_District_Item:
-    def __init__(self, ID):
-        self.ID = ID
+    def __init__(self, dependee = -1):
+        self.x = dependee
+        i = 1
+    
+    @property
+    def area(self): return 0 #currently not computing or using area
+    
+    @property
+    def dependee(self):
+        return self.x
 
+    @dependee.setter
+    def dependee(self, value):
+        i = 1
+        self.x = value
 
 def gen(census_block_plus_district_ID_collection, district_graph):
     for census_block, district_ID in census_block_plus_district_ID_collection:
@@ -20,14 +33,20 @@ def gen(census_block_plus_district_ID_collection, district_graph):
         # that doesn't contain the block centroid -neal
 
         relevant_districts = {district_ID}
-        neighboring_darts = district_graph.vertices[district_ID]
-        for d in neighboring_darts:
-            segment = district_graph.segmentmapper.id2segment[d]
-            if census_block.polygon.intersects(LineString(segment)):
-                neighbor = district_graph.head[district_graph.rev(d)]
-                if neighbor < district_graph.num_vertices()-1:  # if corresponds to a finite cell
-                    relevant_districts.add(neighbor)
-        yield census_block, [Relevant_District_Item(i) for i in relevant_districts]
+        if census_block.block_type != block_types['intersection']:
+            waiting = deque({district_ID})
+            while len(waiting) > 0:
+                v = waiting.pop()
+                neighboring_darts = district_graph.vertices[v]
+                for d in neighboring_darts:
+                    segment = district_graph.id2segment(d)
+                    if census_block.polygon.intersects(LineString(segment)):
+                        neighbor = district_graph.head[district_graph.rev(d)]
+                        if neighbor < district_graph.num_vertices()-1:  # if corresponds to a finite cell
+                            if neighbor not in relevant_districts:
+                                relevant_districts.add(neighbor)
+                                waiting.appendleft(neighbor)
+        yield census_block, {i:Relevant_District_Item() for i in relevant_districts}
 
 
 '''
