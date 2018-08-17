3ae8194b140c865db0552dffb39f30c5a1e31d60
Philip Klein <klein@brown.edu>
2018-08-17 00:27:49 -0400

    minor addition

--- plotting.py
+++ plotting.py
@@ -6,14 +6,16 @@ def find_bbox(pts):
     xmin, ymin, xmax, ymax = [f(g(pt) for pt in pts) for f in [min, max] for g in [(lambda pt:pt[0]), (lambda pt:pt[1])]]
     return (xmin, ymin), (xmax, ymax)
 
-def plot_polygons(polygons):
+def plot_polygons(polygons, bbox=None):
     f, ax = plt.subplots()
-    minpt, maxpt = find_bbox([pt for p in polygons for pt in p.exterior.coords])
-    print("minpt ", minpt, " maxpt", maxpt)
+    minpt, maxpt = find_bbox([pt for p in polygons for pt in p.exterior.coords]) if bbox == None else bbox
+    if bbox == None: print("bounding box:", (minpt, maxpt))
     ax.set_xlim([minpt[0], maxpt[0]])
     ax.set_ylim([minpt[1], maxpt[1]])
     for polygon in polygons:
         ax.add_patch(PolygonPatch(polygon, facecolor='blue', edgecolor='black', alpha=.5))
     plt.show()
 
+
+def plot_polygon(gon): plot_polygons([gon])
 
