10be33c746a0257e4d503d4ce11548da6ee25dd2
Philip Klein <klein@brown.edu>
2018-08-17 00:22:28 -0400

    adding block_type.  Also, new intermediate product---the blocks considered by prepare_ILP, which includes artificial blocks formed by intersecting 0-pop blocks with power cells

--- census_block_file.py
+++ census_block_file.py
@@ -3,8 +3,7 @@ from shapely.geometry import shape, Polygon, MultiPolygon  # ,LineString
 import sys
 import embedded_graph
 from array import array
-
-from census_block import Census_Block
+import census_block
 
 def write_boundary(fout, ring):
     array('d', [coord for pt in ring.coords for coord in pt]).tofile(fout)
@@ -12,52 +11,73 @@ def write_boundary(fout, ring):
 def write_sizes(fout, sizes):
     array('L', sizes).tofile(fout)
 
-def write_census_block(fout, blockID, population, polygon):
-    array('Q', [blockID, population, len(polygon.interiors)]).tofile(fout)
-    #array('L', [population, len(polygon.interiors)]).tofile(fout)
+def write_census_block(fout, blockID, block_type, population, polygon, original_polygon=None):
+    array('L', [blockID, block_type, population, len(polygon.interiors), len(original_polygon.interiors) if original_polygon != None else 0]).tofile(fout)
     write_sizes(fout, [len(polygon.exterior.coords)]+[len(interior.coords) for interior in polygon.interiors])
+    assert census_block.has_original_polygon(block_type) == (original_polygon != None)
+    if original_polygon != None:
+        write_sizes(fout, [len(original_polygon.exterior.coords)]+[len(interior.coords) for interior in original_polygon.interiors])
     write_boundary(fout, polygon.exterior)
     for interior in polygon.interiors:
         write_boundary(fout, interior)
+    if original_polygon != None:
+        write_boundary(fout, original_polygon.exterior)
+        for interior in original_polygon.interiors:
+            write_boundary(fout, interior)
+        
 
 def make_pts(L):
     x_coords = [L[i] for i in range(0, len(L), 2)]
     y_coords = [L[i] for i in range(1, len(L), 2)]
     return list(zip(x_coords, y_coords))
 
+#This uses block IDs from file; can use counter outside if that is preferred
+#    fin = open(filename, 'rb')
+
+def read_block(fin):
+    "might throw EOFError"
+    header = array('L')
+    header.fromfile(fin, 5)
+    blockID = header[0]
+    block_type = header[1]
+    population = header[2]
+    num_interiors = header[3]
+    num_interiors_original = header[4]
+    sizes = array('L')
+    sizes.fromfile(fin, 1+num_interiors)
+    if census_block.has_original_polygon(block_type):
+        original_sizes = array('L')
+        original_sizes.fromfile(fin, 1+num_interiors_original)
+    exterior_coordinates = array('d')
+    exterior_coordinates.fromfile(fin, 2*sizes[0])
+    interiors = []
+    for i in range(num_interiors):
+        interiors.append(array('d'))
+        interiors[i].fromfile(fin, 2*sizes[1+i])
+    ext = make_pts(exterior_coordinates)
+    ints = [make_pts(interior) for interior in interiors]
+    if census_block.has_original_polygon(block_type):
+        original_exterior_coordinates = array('d')
+        original_exterior_coordinates.fromfile(fin, 2*original_sizes[0])
+        original_interiors = []
+        for i in range(num_interiors_original):
+            original_interiors.append(array('d'))
+            original_interiors[i].fromfile(fin, 2*original_sizes[1+i])
+        original_ext = make_pts(exterior_coordinates)
+        original_ints = [make_pts(interior) for interior in interiors]
+    if census_block.has_original_polygon(block_type):
+        return census_block.Subblock(blockID, block_type, Polygon(ext, ints), Polygon(original_ext, original_ints))
+    else:
+        return census_block.Census_Block(blockID, block_type, Polygon(ext, ints), population)
+
 def read(filename):
     fin = open(filename, 'rb')
-    counter = 0
     while True:
-        header = array('Q')
         try:
-            header.fromfile(fin, 3)
+            b = read_block(fin)
+            yield b
         except EOFError:
             break
-        blockID = header[0]
-        population = header[1]
-        num_interiors = header[2]
-        sizes = array('L')
-        try:
-            sizes.fromfile(fin, 1+num_interiors)
-        except EOFError:
-            print("sizes")
-        exterior_coordinates = array('d')
-        try:
-            exterior_coordinates.fromfile(fin, 2*sizes[0])
-        except EOFError:
-            print("exterior")
-        interiors = []
-        for i in range(num_interiors):
-            interiors.append(array('d'))
-            try:
-                interiors[i].fromfile(fin, 2*sizes[1+i])
-            except EOFError:
-                print("interior ",i)
-        ext = make_pts(exterior_coordinates)
-        ints = [make_pts(interior) for interior in interiors]
-        yield Census_Block(counter, Polygon(ext, ints), population)
-        counter += 1
 
 if __name__ == "__main__":
     import time
