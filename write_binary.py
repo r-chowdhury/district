20d72a3cf6adc0045371e24be5daad9e4d132fe3
megangessner <megan_gessner@brown.edu>
2018-08-28 23:04:21 -0700

    The script takes in input files for do_redistrict, split_pulp, and prepare_ILP and output path destinations for the district polygon and census polygon files. If not given any of those files, it defaults to a set up assuming the directory has a binary_data subdir and makefile_outputs subdir, for ease of generating files. Command format is 'python3 write_binary.py <state_abbrev> [do_redistrict_out] [split_pulp_out] [prepare_ILP_blockdata_out] [district_polygon_path] [census_polygon_path]'.

--- write_binary.py
+++ write_binary.py
@@ -5,8 +5,6 @@ import Voronoi_boundaries as vb
 import scipy.spatial as sp
 import shapely.geometry as sg
 import census_polygons as cp
-import split_pulp as sp
-import district_graph as dg
 from functools import reduce
 
 def getCoords(power_cell):
@@ -14,64 +12,52 @@ def getCoords(power_cell):
 	return list(power_cell.exterior.coords)
 
 
-#this function is no longer needed
-def getDiffCoords(power_cell):
-
-	diff = reduce(lambda x,y: x.difference(y), all_blocks, power_cell) #this function takes a lot of time
-
-	print("calculated diff!")
-
-
-	if (not isinstance(diff, list)):
-		diff_coords = getCoords(diff)
-
-	else: 
-		diff_coords = map(getCoords, diff)
-
-	return list(diff_coords)
-
-
-def vb_helper(output_filename):
-	#used to be required for read function in cb... may not be needed anymore
-
-	C, A, assign_pairs = vb.Parse(do_redistrict_out)
-	bbox = vb.find_bbox(C+A)
-	G = dg.get(C, power_cells, bbox)
-
-	return C, G
-
-
 
 if __name__ == '__main__':
 
-	'''Format python3 do_redistrict2.py [state abbrev]'''
-
+	'''Format python3 do_redistrict2.py [state abbrev] [split_pulp makefile output] [do_redistrict makefile output] [prepare_ILP makefile output]
+		[district polygon output path] [census block polygon output path]''' 
 
 	state_abbrev = sys.argv[1]
 
-	power_cells = vb.power_cells_fromfile("./makefile_outputs/do_redistrict/"+state_abbrev)
+	if (len(sys.argv) == 2):
+		#this assumes that this script is in the same directory as the makefile outputs
+		do_redistrict_out = "./makefile_outputs/do_redistrict/"+state_abbrev
+		split_pulp_out = "./makefile_outputs/split_pulp/"+state_abbrev
+		prepare_ILP_out = "./makefile_outputs/prepare_ILP/"+state_abbrev+"_blockdata"
+		district_polygon_path = "./binary_data/district_polygons/polygons_"+state_abbrev
+		census_polygon_path = './binary_data/census_polygons/boundary_blocks_'+state_abbrev
 
-	#C, G = vb_helper(do_redistrict_out)
+	else:
+		args = sys.argv[2:]
+		do_redistrict_out = args.pop()
+		split_pulp_out = args.pop()
+		prepare_ILP_out = args.pop()
+		district_polygon_path = args.pop()
+		census_polygon_path = args.pop()
+
+	state_abbrev = sys.argv[1]
+
+
+
+	power_cells = vb.power_cells_fromfile(do_redistrict_out)
 
 	boundary_block_assignments = cp.parse_polygons(
-										"./makefile_outputs/split_pulp/"+state_abbrev, #output file from split_pulp
-										"./makefile_outputs/prepare_ILP/"+state_abbrev+"_blockdata", #census block boundary shapefile
+										split_pulp_out, #output file from split_pulp
+										prepare_ILP_out, #census block boundary shapefile
 										)
 
 	all_blocks = cp.polygon_list(boundary_block_assignments)
 
 	full_coords = map(getCoords, power_cells) #full outline of district
-	#diff_coords = map(getDiffCoords, power_cells) #district with census blocks subtracted out
 
 
-	poly_file = open("./binary_data/district_polygons/polygons_"+state_abbrev, "wb+")
-	#poly_file.write(str(list(full_coords)))
+	poly_file = open(district_polygon_path, "wb+")
 	poly_file.write(str(list(full_coords)).encode('utf-8'))
 	poly_file.close()
 
 
-	district_file = open('./binary_data/census_polygons/boundary_blocks_'+state_abbrev, "wb+")
-	#print(json.dumps(boundary_block_assignments), file=district_file)
+	district_file = open(census_polygon_path, "wb+")
 	district_file.write(json.dumps(boundary_block_assignments).encode('utf-8'))
 	district_file.close()
 
