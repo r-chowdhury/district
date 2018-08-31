c33c92d7b1d1ceabe4ae1e20f37789ea18be4b88
Philip Klein <klein@brown.edu>
2018-08-31 15:33:41 -0400

    getting rid of having an 'original' polygon; including home district---also introducing convenient procedure for writing a block object

--- census_block_file.py
+++ census_block_file.py
@@ -11,20 +11,15 @@ def write_boundary(fout, ring):
 def write_sizes(fout, sizes):
     array('L', sizes).tofile(fout)
 
-def write_census_block(fout, blockID, block_type, population, polygon, original_polygon=None):
-    array('L', [blockID, block_type, population, len(polygon.interiors), len(original_polygon.interiors) if original_polygon != None else 0]).tofile(fout)
+def write_census_block(fout, blockID, block_type, population, polygon, home_district=None):
+    array('L', [blockID, block_type, population, len(polygon.interiors), home_district+1 if home_district != None else 0]).tofile(fout)
     write_sizes(fout, [len(polygon.exterior.coords)]+[len(interior.coords) for interior in polygon.interiors])
-    assert census_block.has_original_polygon(block_type) == (original_polygon != None)
-    if original_polygon != None:
-        write_sizes(fout, [len(original_polygon.exterior.coords)]+[len(interior.coords) for interior in original_polygon.interiors])
     write_boundary(fout, polygon.exterior)
     for interior in polygon.interiors:
         write_boundary(fout, interior)
-    if original_polygon != None:
-        write_boundary(fout, original_polygon.exterior)
-        for interior in original_polygon.interiors:
-            write_boundary(fout, interior)
-        
+
+def write(fout, block):
+    write_census_block(fout, block.ID, block.block_type, block.population, block.polygon, block.home_district if hasattr(block, "home_district") else None)
 
 def make_pts(L):
     x_coords = [L[i] for i in range(0, len(L), 2)]
@@ -42,12 +37,9 @@ def read_block(fin):
     block_type = header[1]
     population = header[2]
     num_interiors = header[3]
-    num_interiors_original = header[4]
+    home_district = header[4] - 1
     sizes = array('L')
     sizes.fromfile(fin, 1+num_interiors)
-    if census_block.has_original_polygon(block_type):
-        original_sizes = array('L')
-        original_sizes.fromfile(fin, 1+num_interiors_original)
     exterior_coordinates = array('d')
     exterior_coordinates.fromfile(fin, 2*sizes[0])
     interiors = []
@@ -56,19 +48,7 @@ def read_block(fin):
         interiors[i].fromfile(fin, 2*sizes[1+i])
     ext = make_pts(exterior_coordinates)
     ints = [make_pts(interior) for interior in interiors]
-    if census_block.has_original_polygon(block_type):
-        original_exterior_coordinates = array('d')
-        original_exterior_coordinates.fromfile(fin, 2*original_sizes[0])
-        original_interiors = []
-        for i in range(num_interiors_original):
-            original_interiors.append(array('d'))
-            original_interiors[i].fromfile(fin, 2*original_sizes[1+i])
-        original_ext = make_pts(exterior_coordinates)
-        original_ints = [make_pts(interior) for interior in interiors]
-    if census_block.has_original_polygon(block_type):
-        return census_block.Subblock(blockID, block_type, Polygon(ext, ints), Polygon(original_ext, original_ints))
-    else:
-        return census_block.Census_Block(blockID, block_type, Polygon(ext, ints), population)
+    return census_block.Census_Block(blockID, block_type, Polygon(ext, ints), population, home_district)
 
 def read(filename):
     fin = open(filename, 'rb')
